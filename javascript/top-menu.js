5fd034131f4eafb24b890b246d90cbfb19c557cd
BRUNA <brutiko@gmail.com>
2017-01-22 13:24:33 +0100

    Top menu changes on scroll down.

--- javascript/top-menu.js
+++ javascript/top-menu.js
@@ -5,9 +5,14 @@ $(document).ready(function(){
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $('#navbar').css({'background-color':'#44384f','color':'#333333'});
+          $('#navbar').css('background-color','#44384f');
+          $('#navbar').addClass('navbar-brand-scroll');
+          $('.logo').css('margin', '10px 0');
        } else {
           $('#navbar').css('background-color', 'transparent');
+          $('#navbar').removeClass('navbar-brand-scroll');
+          $('.logo').css('margin', '20px 0');
        }
    });
 });
+
