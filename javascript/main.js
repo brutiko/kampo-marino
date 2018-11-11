212a68d8dc6fb10f9db3bbb849aadcc8dbe793e5
BRUNA <brutiko@gmail.com>
2018-11-11 17:26:54 +0100

    [Products] Button 'show more' removed.

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,13 @@
-$(function () {
-    $(".product-thumb-item").slice(0, 12).show();
-    $("#loadMore").on('click', function (e) {
-        e.preventDefault();
-        $(".product-thumb-item:hidden").slice(0, 6).slideDown();
-        if ($(".product-thumb-item:hidden").length == 0) {
-            $("#load").fadeOut('slow');
-        }
-        $('html,body').animate({
-            scrollTop: $(this).offset().top
-        }, 1500);
-    });
-});
+// $(function () {
+//     $(".product-thumb-item").slice(0, 12).show();
+//     $("#loadMore").on('click', function (e) {
+//         e.preventDefault();
+//         $(".product-thumb-item:hidden").slice(0, 6).slideDown();
+//         if ($(".product-thumb-item:hidden").length == 0) {
+//             $("#load").fadeOut('slow');
+//         }
+//         $('html,body').animate({
+//             scrollTop: $(this).offset().top
+//         }, 1500);
+//     });
+// });
